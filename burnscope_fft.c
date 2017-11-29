381c519dd0489c6cab6fd5639abf4d967c648836
Neels Hofmeyr <neels@hofmeyr.de>
2017-11-29 05:57:52 +0100

    Revert "adjust game controller due to some upstream change (some axes became hats)"
    This reverts commit 3e213ef1cf80d5453ba5625da00a76fca59056e8.

--- burnscope_fft.c
+++ burnscope_fft.c
@@ -1918,6 +1918,37 @@ int main(int argc, char *argv[])
 
               // these axes select the joystick layers
               int l = ctrl->selected_layer;
+              switch(event.jaxis.axis)
+              {
+                case 6:
+                  if (axis_val < -.2) {
+                    l = 2;
+                  }
+                  else
+                  if (axis_val > .2) {
+                    l = 3;
+                  }
+                  break;
+
+                case 7:
+                  if (axis_val < -.2) {
+                    l = 1;
+                  }
+                  else
+                  if (axis_val > .2) {
+                    l = 0;
+                  }
+                  break;
+
+                default:
+                  break;
+              }
+
+              if (l != ctrl->selected_layer) {
+                // layer changed
+                ctrl->selected_layer = l;
+                printf("LAYER %d %f\n", ctrl->selected_layer, axis_val);
+              }
 
               switch (l)
               {
@@ -2259,34 +2290,8 @@ int main(int argc, char *argv[])
             break;
 
           case SDL_JOYHATMOTION:  /* Handle Hat Motion */
-            {
-              controller_state_t *ctrl = &controller[event.jhat.which];
-              int l = ctrl->selected_layer;
-              switch(event.jhat.value)
-              {
-                case 1:
-                  l = 0;
-                  break;
-                case 2:
-                  l = 1;
-                  break;
-                case 4:
-                  l = 2;
-                  break;
-                case 8:
-                  l = 3;
-                  break;
-
-                default:
-                  break;
-              }
-
-              if (l != ctrl->selected_layer) {
-                // layer changed
-                ctrl->selected_layer = l;
-                printf("LAYER %d\n", ctrl->selected_layer);
-              }
-            }
+            printf("%2d: hat %d = %d\n",
+                   event.jhat.which, event.jhat.hat, event.jhat.value);
             break;
 
           case SDL_JOYBALLMOTION:  /* Handle Joyball Motion */
